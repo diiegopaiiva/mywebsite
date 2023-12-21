04a2356591c45c39d3b511cf04bd12c5e7c25bae
Diego Paiva <www.diegopaiva@gmail.com>
2023-12-21 18:07:46 -0300

    chang project

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -33,7 +33,7 @@ const Login = () => {
   return (
     <div className="login-container">
       <div className="login-header">
-        <h1>Facebook</h1>
+        <h1>GeekGrove</h1>
       </div>
       <div className="login-form">
         <form onSubmit={handleSubmit}>
